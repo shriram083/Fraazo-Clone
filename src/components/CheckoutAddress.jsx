e8c78813cfde06cf9d217092787d4594c91576d2
Shriram <shriramdeshpande77@gmail.com>
2022-06-16 22:45:18 +0530

    drawer added to checkout adress and some css modified

--- src/components/CheckoutAddress.jsx
+++ src/components/CheckoutAddress.jsx
@@ -8,22 +8,64 @@ import {
   DrawerOverlay,
   Heading,
   Image,
+  Radio,
+  RadioGroup,
+  Stack,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import map from "../assets/mapImage.png";
 import searchAddress from "../assets/searchAddress.png";
-import { TbCurrencyRupee, TbDiscount2 } from "react-icons/tb";
+import { TbCurrencyRupee } from "react-icons/tb";
+import { FiMoreVertical } from "react-icons/fi";
 import { TiLocation } from "react-icons/ti";
 import { GrLocation } from "react-icons/gr";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { MdOutlineAddLocationAlt } from "react-icons/md";
-import { ArrowBackIcon, ChevronLeftIcon } from "@chakra-ui/icons";
-
+import {
+  ArrowBackIcon,
+  CheckCircleIcon,
+  ChevronLeftIcon,
+} from "@chakra-ui/icons";
+import styled from "styled-components";
+const MyInput = styled.input`
+  border: 1px solid gray;
+  padding: 15px;
+  margin: 10px auto;
+  width: 400px;
+  display: block;
+  border-radius: 5px;
+`;
 const CheckoutCart = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [drawer, setDrawer] = useState(false);
+  const [formData, setFormData] = useState({
+    completeAddress: "",
+    flatNo: "",
+    building: "",
+    howToReach: "",
+    type: "",
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const hadnleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      completeAddress: "",
+      flatNo: "",
+      building: "",
+      howToReach: "",
+      type: "",
+    });
+    setDrawer(false);
+    onClose();
+  };
   return (
     <div>
       <Box
@@ -103,32 +145,163 @@ const CheckoutCart = () => {
           size={"sm"}
         >
           <DrawerOverlay />
-          <DrawerContent>
-            <DrawerHeader borderBottomWidth="1px" p="0px">
-              <Image src={map} />
-              <ArrowBackIcon
-                onClick={onClose}
-                fontSize="30px"
-                position="absolute"
-                top="12px"
-                left="12px"
-                cursor="pointer"
-              />
-            </DrawerHeader>
-            <DrawerBody padding="0px">
-              <Image src={searchAddress} />
-              <Text fontSize="18px" fontWeight="500" margin="20px">
-                Select Delivery Address
-              </Text>
-              <Box color="#47be9b" margin="20px" gap="20px" display="flex">
-                <AiFillPlusCircle fontSize="30px" />
-                <Text>Add Address</Text>
-              </Box>
-              <Box>
-                <GrLocation />
-              </Box>
-            </DrawerBody>
-          </DrawerContent>
+          {!drawer ? (
+            <DrawerContent>
+              <DrawerHeader borderBottomWidth="1px" p="0px">
+                <Image src={map} />
+                <ArrowBackIcon
+                  onClick={onClose}
+                  fontSize="30px"
+                  position="absolute"
+                  top="12px"
+                  left="12px"
+                  cursor="pointer"
+                />
+              </DrawerHeader>
+              <DrawerBody padding="0px">
+                <Image src={searchAddress} />
+                <Text fontSize="18px" fontWeight="500" margin="20px">
+                  Select Delivery Address
+                </Text>
+                <Box
+                  color="#47be9b"
+                  margin="20px"
+                  gap="20px"
+                  display="flex"
+                  cursor="pointer"
+                  onClick={() => setDrawer(true)}
+                >
+                  <AiFillPlusCircle fontSize="30px" />
+                  <Text>Add Address</Text>
+                </Box>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  borderTop="1px solid black"
+                  borderBottom="1px solid black"
+                  padding="20px 10px"
+                >
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="start"
+                    gap="20px"
+                  >
+                    <GrLocation />
+                    <Box margin="-10px 0px">
+                      <Text>abc</Text>
+                      <Text>1,2,3</Text>
+                    </Box>
+                    <CheckCircleIcon
+                      color="#47be9b"
+                      backgroundColor="#eff9f7"
+                    />
+                  </Box>
+                  <FiMoreVertical />
+                </Box>
+              </DrawerBody>
+            </DrawerContent>
+          ) : (
+            <DrawerContent>
+              <DrawerHeader borderBottomWidth="1px" p="0px">
+                <ArrowBackIcon
+                  onClick={() => setDrawer(false)}
+                  fontSize="30px"
+                  position="absolute"
+                  top="12px"
+                  left="12px"
+                  cursor="pointer"
+                />
+              </DrawerHeader>
+              <DrawerBody padding="0px">
+                <Heading margin="20px" marginTop="50px" fontSize="20px">
+                  Address
+                </Heading>
+                <form onSubmit={hadnleSubmit}>
+                  <MyInput
+                    type="text"
+                    placeholder="Complete Address..."
+                    name="completeAddress"
+                    value={formData.completeAddress}
+                    onChange={handleChange}
+                    style={{ padding: "30px 10px" }}
+                  />
+                  <MyInput
+                    type="text"
+                    placeholder="Door / Flat No."
+                    name="flatNo"
+                    value={formData.flatNo}
+                    onChange={handleChange}
+                  />
+                  <MyInput
+                    type="text"
+                    placeholder="Building"
+                    name="building"
+                    value={formData.building}
+                    onChange={handleChange}
+                  />
+                  <MyInput
+                    type="text"
+                    placeholder="How to Reach"
+                    name="howToReach"
+                    value={formData.howToReach}
+                    onChange={handleChange}
+                  />
+                  <RadioGroup
+                    name="type"
+                    onChange={(e) => setFormData({ ...formData, type: e })}
+                  >
+                    <Stack
+                      spacing={4}
+                      direction="row"
+                      display="flex"
+                      justifyContent="center"
+                      gap="10px"
+                      padding="10px"
+                    >
+                      <Box
+                        border="1px solid gray"
+                        padding="5px 16px"
+                        borderRadius="5px"
+                      >
+                        <Radio value="Home">Home</Radio>
+                      </Box>
+                      <Box
+                        border="1px solid gray"
+                        padding="5px 16px"
+                        borderRadius="5px"
+                      >
+                        <Radio value="Office">Office</Radio>
+                      </Box>
+                      <Box
+                        border="1px solid gray"
+                        padding="5px 16px"
+                        borderRadius="5px"
+                      >
+                        <Radio value="Other">Other</Radio>
+                      </Box>
+                    </Stack>
+                  </RadioGroup>
+                  <button
+                    style={{
+                      backgroundColor: "#43c6ac",
+                      color: "white",
+                      width: "400px",
+                      padding: "20px 20px 20px 50px",
+                      display: "flex",
+                      margin: "10px auto",
+                      fontSize: "18px",
+                      fontWeight: "500",
+                      borderRadius: "10px",
+                      textAlign: "center",
+                    }}
+                  >
+                    SAVE ADDRESS AND PROCEED
+                  </button>
+                </form>
+              </DrawerBody>
+            </DrawerContent>
+          )}
         </Drawer>
 
         <Box
